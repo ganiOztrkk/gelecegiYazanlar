964c376edfaf8f8fb62c50eca30e3b282fd12319
Gani Ozturk <ganioztrk@gmail.com>
2023-03-09 00:54:52 +0300

    Added first week codes

---
+++ C#Basics/1stWeek/TryAndCatch-Ders2/TryAndCatch/Program.cs
@@ -0,0 +1,43 @@
+﻿// var keywordunun amacı anonim veri tiplerini tutmaktır. bir veriyi karşılarken hangi veri gelecek bilmiyorsak buna keywordun karar vermesini saglarız. 
+
+//Console.WriteLine("bir sayı giriniz");
+//var sayi1 = Convert.ToInt32(Console.ReadLine());
+//Console.WriteLine("bir sayı giriniz");
+//var sayi2 = Convert.ToInt32(Console.ReadLine());
+//int bolum = sayi2 / sayi2;
+//Console.WriteLine($"islem sonucu {bolum}"); // basa koydugumuz isaret interpolition - cw scope icerisine degiskeni direkt olarak konumlamamızı saglar.
+
+//peki ergenin biri çıkagelip sayı girecegi yere metin girerse ne olur ? 
+//program patlar. bize bir exception fırlatır. kullanıcıya bu durumu yansıtmamak icin try catch kullanırız.
+// aynı islemi patlamayacak sekilde tekrar yapalım
+
+try
+{
+    Console.WriteLine("bir sayı giriniz");
+    var sayi1 = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("bir sayı giriniz");
+    var sayi2 = Convert.ToInt32(Console.ReadLine());
+    int bolum = sayi2 / sayi2;
+    Console.WriteLine($"islem sonucu {bolum}");
+}
+catch (FormatException)
+{
+    Console.WriteLine("ergen kardesim çıkıklık yapma.");
+}
+//bir işlem yapısında birden cok hata durumu olabilir. mesela işlemi 0 ile yapmak gibi. bu yuzden alt alta farklı catch blokları oluşturabiliriz.
+catch (DivideByZeroException)
+{
+    Console.WriteLine("sıfıra sayı mı bölünür ergen kardesim");
+}
+//peki tahmin bile edemeyecegimiz bir hata olursa ne olacak ? cok genel ve hepsini kapsayan bir exception ile bunları yakalayabiliriz.
+catch (Exception exception)
+{
+    Console.WriteLine($"kim bilir ne sacmalık yaptın ergen kardesim {exception.Message}");
+}
+//hatayı yakalık. peki hata alındı ama hata alınsa dahi her halukarda bir kod satırının calısmasını istiyorsak ? bu kez en son finally kullanıyoruz
+finally
+{
+    Console.WriteLine("burası her halukarda calısacak.");
+}
+
+// goto fonksiyonunu gorsen de bilsen de kullanma. var ama bilmezden geliyoruz. goto bir kakadır! goto kullanma döngü kullan.
---
+++ C#Basics/1stWeek/VariablesAndTypes-Ders2/VariablesAndTypes/Program.cs
@@ -0,0 +1,55 @@
+﻿/*
+ *sayısal veri tipleri
+ *-tam sayı
+ *-ondalıklı sayı
+ *
+ *
+ *metinsel veri tipler
+ *
+ *mantıksal veri tipler
+ *
+ */
+
+byte enKucuk = 255;
+sbyte eksiOlabilenByte = - 128;
+short onaltiBit= 32767;
+ushort sadecePozitif = 65535;
+ulong bayagiBuyukTamSayi = ulong.MaxValue;
+
+// 8 - 16 - 32 - 64  bitlik alanlar kaplarlar
+//s - u - u - u
+
+//c# daki evrensel ondalıklı deger double'dır. bu dilde ondalıklı bir deger gordugumuzde cok yuksek ihtimal bu sayı doubledır.
+
+double pi = 3.14;
+float piFloat = 3.14f;
+decimal  piDecimal = 3.14000000000000000000000000000000000000000000000000000000000000000000m;
+
+
+//sözel
+
+char symbol = '!';
+string name = "Türkay"; // string aslında char koleksiyonudur. Turkay
+
+String a = "kelime";
+string b = "kelime";
+//ikisi arasındaki String .net e ait bir classtır. string ise c# diline ait bir keyworddur. buyuk harfli degiskenler .net e aittir. performanstan metoda her seyi aynıdır. tek fark biri .net e ait. System.Decimal gibi digeriyse dile ait keyworddur. decimal gibi.
+
+//mantıksal - true false
+bool olurMu = false;
+
+byte x = 255;
+byte y = 2;
+checked // bu metot ile işlemin hata fırlatmasını sağlarız
+{
+	byte result = (byte)(x + y);
+	Console.WriteLine(result); 
+}
+//bu islem casting islemidir. yani bir donusturme islemidir. elde ettigimiz result verisini tekrar byte turune cast etmis bulunuyoruz. bu durum hata fırlatmaz fakat tehlikelidir.
+//cunku veri hafıza sınırı aşıldığı için result tekrar 0a donecek ve buradan eklemeye devam edecektir. yukarıdaki islemin sonucu 1 cıktısını verir.
+
+//-------------------------------------------------------------
+
+// kullanıcıdan veri girdisi yapılan herrrrrr sey stringtir. bu hangi arayuzde olursa olsun, kullanıcı veri giriyorsa bu veri stringtir bu bir standarttır.
+
+
