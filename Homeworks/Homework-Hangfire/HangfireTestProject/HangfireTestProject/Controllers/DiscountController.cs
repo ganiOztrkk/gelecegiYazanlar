c2376893a12ae9b8aa440f6de17ecb9dd806265c
Gani Ozturk <ganioztrk@gmail.com>
2023-06-10 00:56:23 +0300

    Hangfire Exercise

---
+++ Homeworks/Homework-Hangfire/HangfireTestProject/HangfireTestProject/Controllers/DiscountController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Hangfire;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HangfireTestProject.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DiscountController : ControllerBase
+    {
+        [HttpPost]
+        [Route("[action]")]
+        public IActionResult Discount()
+        {
+            BackgroundJob.Schedule(() => Ok(), TimeSpan.FromSeconds(10));
+            return Ok("Delayed Job Çalıştı.");
+        }
+    }
+}
---
+++ Homeworks/Homework-Hangfire/HangfireTestProject/HangfireTestProject/Controllers/UserController.cs
@@ -0,0 +1,19 @@
+﻿using Hangfire;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HangfireTestProject.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        [HttpPost]
+        [Route("[action]")]
+        public IActionResult SignUp()
+        {
+            BackgroundJob.Enqueue(() => Ok());
+            return Ok("Fire and Forget Tetiklendi");
+        }
+    }
+}
